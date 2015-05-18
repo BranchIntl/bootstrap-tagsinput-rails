2d349d439fd41c1b74113467fed47bbfd94a1263
Randy Bares <aeramor@gmail.com>
2015-05-18 14:30:57 -0700

    Better class handling

--- vendor/assets/javascripts/bootstrap-tagsinput.js
+++ vendor/assets/javascripts/bootstrap-tagsinput.js
@@ -40,8 +40,9 @@
     this.objectItems = options && options.itemValue;
     this.placeholderText = element.hasAttribute('placeholder') ? this.$element.attr('placeholder') : '';
     this.inputSize = Math.max(1, this.placeholderText.length);
-
-    this.$container = $('<div class="bootstrap-tagsinput"></div>');
+    this.extraClass = element.hasAttribute('class') ? this.$element.attr('class') : '';
+    
+    this.$container = $('<div class="' + this.extraClass + ' bootstrap-tagsinput"></div>');
     this.$input = $('<input type="text" placeholder="' + this.placeholderText + '"/>').appendTo(this.$container);
 
     this.$element.before(this.$container);
